68e26e751350bfd7005b811a0db321258b047154
deshankoswatte <dehami.deshan@gmail.com>
2020-05-24 15:00:02 +0530

    Added implementation to generate SAML Assertions

---
+++ src/main/java/org/wso2/carbon/sts/rest/server/custom/PasswordCallbackHandler.java
@@ -0,0 +1,34 @@
+package org.wso2.carbon.sts.rest.server.custom;
+
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+public class PasswordCallbackHandler implements CallbackHandler {
+
+    public void handle(Callback[] callbacks) throws IOException,
+            UnsupportedCallbackException {
+        for (Callback callback : callbacks) {
+            if (callback instanceof WSPasswordCallback) { // CXF
+                WSPasswordCallback pc = (WSPasswordCallback) callback;
+                if ("alice".equals(pc.getIdentifier())) {
+                    pc.setPassword("clarinet");
+                    break;
+                } else if ("bob".equals(pc.getIdentifier())) {
+                    pc.setPassword("trombone");
+                    break;
+                } else if ("mystskey".equals(pc.getIdentifier())) {
+                    pc.setPassword("stskpass");
+                    break;
+                } else if ("myservicekey".equals(pc.getIdentifier())) {
+                    pc.setPassword("skpass");
+                    break;
+                }
+            }
+        }
+    }
+}
